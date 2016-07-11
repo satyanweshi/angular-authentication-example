82fe751850000b8c6cd32be84609a2fb0283e46a
Varun Sharma <varun.sharma2@xerox.com>
2016-07-11 15:07:05 +0530

    Final code that works for authentication and is also in the format suggested by the angular website

--- app/app.js
+++ app/app.js
@@ -2,13 +2,37 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
+  'Authentication',
+  'Home',
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ngCookies'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+  		.when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'authentication/authentication.template.html'
+        })
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'home/home.template.html'
+        })
+  		.otherwise({redirectTo: '/login'});
+}])
+.run(['$rootScope', '$location', '$cookieStore', '$http',
+      function ($rootScope, $location, $cookieStore, $http) {
+    // keep user logged in after page refresh
+    $rootScope.globals = $cookieStore.get('globals') || {};
+    if ($rootScope.globals.currentUser) {
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+    }
+
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        // redirect to login page if not logged in
+        if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+            $location.path('/login');
+        }
+    });
 }]);
