971285a5e6d44677175aa96fbc6c4169dd4073e5
Varun Sharma <varun.sharma2@xerox.com>
2016-07-11 11:20:54 +0530

    Made the branch appear similar to angular seed. Next step. Merge the authentication code.

--- app/app.js
+++ app/app.js
@@ -1,40 +1,14 @@
 'use strict';
 
-angular.module('BasicHttpAuthExample', [
-    'Authentication',
-    'Home',
-    'ngRoute',
-    'ngCookies'
-])
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
 
-.config(['$routeProvider', function ($routeProvider) {
-
-    $routeProvider
-        .when('/login', {
-            controller: 'LoginController',
-            templateUrl: 'authentication/views/login.html'
-        })
-
-        .when('/', {
-            controller: 'HomeController',
-            templateUrl: 'home/views/home.html'
-        })
-
-        .otherwise({ redirectTo: '/login' });
-}])
-
-.run(['$rootScope', '$location', '$cookieStore', '$http',
-    function ($rootScope, $location, $cookieStore, $http) {
-        // keep user logged in after page refresh
-        $rootScope.globals = $cookieStore.get('globals') || {};
-        if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-        }
-
-        $rootScope.$on('$locationChangeStart', function (event, next, current) {
-            // redirect to login page if not logged in
-            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
-                $location.path('/login');
-            }
-        });
-    }]);
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
